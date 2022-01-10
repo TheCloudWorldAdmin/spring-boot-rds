ef226371b9883e35dc3dbfd4d97159e3d6539c9d
wilshan <74950445+wilshan@users.noreply.github.com>
2022-01-10 16:05:43 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -xe
 yum update -y
-wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-yum install -y apache-maven
-yum install java-1.8.0-openjdk-devel.x86_64 -y
-yum install zip -y
-yum install git -y
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+sudo yum install java-1.8.0-openjdk-devel.x86_64 -y
+sudo yum install zip -y
+sudo yum install git -y
 git clone https://github.com/wilshan/spring-boot-rds.git
 cd spring-boot-rds
 mvn clean install
